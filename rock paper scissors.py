5f2bc53e31be030f54b508b381bfe1ae15abf04e
Pozik7 <leoleo4821@gmail.com>
2024-01-06 15:35:30 +0200

    improvement and ASCII work

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,5 +1,34 @@
 import random
 
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+game_img = [rock, paper, scissors]
+
 user_choice = input("What do you choose, Rock Paper or Scissors?\n").lower()
 
 valid_choice = ["rock", "paper", "scissors"]
@@ -7,14 +36,14 @@ valid_choice = ["rock", "paper", "scissors"]
 
 if user_choice in valid_choice:
     print("Your Choice:", user_choice)
+    print(game_img[valid_choice.index(user_choice)])
 else:
     print("Invalid choice. Please enter rock, paper, or scissors.")
 
 
-choices = ["rock", "paper", "scissors"]
-computer_choice = random.choice(choices)
+computer_choice = random.choice(valid_choice)
 print("Computer chose:", computer_choice)
-
+print(game_img[valid_choice.index(computer_choice)])
 
 if user_choice == computer_choice:
     print("It's a tie!")
